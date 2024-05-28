8ec8e6442bcd4780b8aac06cdacdb62390cdb676
Oz174 <ahmedtarek1754@gmail.com>
2024-05-29 00:58:28 +0300

    Day 12 finally

---
+++ sql/groupingsets_cube_rollup.sql
@@ -0,0 +1,21 @@
+-- Grouping Sets
+-- returns sum by city in n city rows and m fruit rows
+Select Fruit , City , sum(Amt) 
+from Spends
+group By Grouping SETS ((Fruit) , (City));
+
+-- Rollup
+-- returns sum of the first argument in n rows of the first argument
+-- returns sum of first arg N second argument (as if it accumulated)
+-- returns the total also at the end 
+Select Fruit , City , sum(Amt)
+from Spends
+group By Rollup (Fruit , City); -- or city , fruit
+
+-- Cube
+-- returns sum of the first argument in n rows of the first argument
+-- for A , B , C it returns sum of A , B , C , A.B , A.C , B.C , A.B.C
+-- returns total also 
+Select Fruit , City , sum(Amt)
+from Spends
+group By Cube (Fruit , City); -- or city , fruit
