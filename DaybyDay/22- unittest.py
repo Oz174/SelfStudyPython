6c32b84f18c275faa961474ae583b78374491f14
Oz174 <ahmedtarek1754@gmail.com>
2024-06-23 11:24:23 +0300

    added validate email test

--- DaybyDay/22- unittest.py
+++ DaybyDay/22- unittest.py
@@ -1,4 +1,5 @@
 import unittest
+import re
 
 def add(x, y):
     return x + y
@@ -7,6 +8,15 @@ class AddTest(unittest.TestCase):
     def test_add(self):
         self.assertEqual(add(3, 4), 7)
         self.assertEqual(add(-2, -3), -5)
+
+def validate_email(email):
+    return True if re.search(r'^\w+@\w+\.\w+', email) else False
+
+class EmailTest(unittest.TestCase):
+    def test_email(self):
+        self.assertTrue(validate_email('ahmedtarek1754@gmail.com'))
+        self.assertFalse(validate_email('ahmedtarek1754gmail.com'))
+        self.assertFalse(validate_email('ahmedtarek1754@gmailcom'))
 
 if __name__ == "__main__":
     unittest.main()
