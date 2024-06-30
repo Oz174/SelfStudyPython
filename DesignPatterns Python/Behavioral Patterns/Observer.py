fb10e83d3172635a18cbdde8c04e1b8a2407c09b
Oz174 <ahmedtarek1754@gmail.com>
2024-06-30 10:59:31 +0300

    modifications to design patterns

--- DesignPatterns Python/Behavioral Patterns/Observer.py
+++ DesignPatterns Python/Behavioral Patterns/Observer.py
@@ -1,41 +1,58 @@
-class Subject:
+class Inventory:
     def __init__(self):
-        self.__observers = []
+        self.observers = []
+        self._product = None
+        self._quantity = 0
 
-    def register_observer(self, observer):
-        self.__observers.append(observer)
+    def attach(self, observer):
+        self.observers.append(observer)
 
-    def notify_observers(self, message):
-        for observer in self.__observers:
-            observer.notify(message)
+    @property
+    def product(self):
+        return self._product
+
+    @product.setter
+    def product(self, value):
+        self._product = value
+        self._update_observers()
+
+    @property
+    def quantity(self):
+        return self._quantity
+
+    @quantity.setter
+    def quantity(self, value):
+        self._quantity = value
+        self._update_observers()
+
+    def _update_observers(self):
+        for observer in self.observers:
+            observer()
 
 
-class Observer:
-    def notify(self, message):
-        pass
+class ConsoleObserver:
+    def __init__(self, inventory):
+        self.inventory = inventory
 
-
-class EmailAlerts(Observer):
-    def notify(self, message):
-        print(f"Email Alert: {message}")
-
-
-class SMSAlerts(Observer):
-    def notify(self, message):
-        print(f"SMS Alert: {message}")
+    def __call__(self):
+        if self.inventory.quantity > 4:
+            print(f"{self.inventory.product} has {self.inventory.quantity}, overload warning!")
+        print(self.inventory.product)
+        print(self.inventory.quantity)
 
 
 if __name__ == "__main__":
-    print("Welcome to Observer Pattern")
-    print("This class aims to notify all the observers when a change occurs in the subject")
-    subject = Subject()  # => to be observed
-    email_alerts = EmailAlerts()  # => observer
-    sms_alerts = SMSAlerts()  # => observer
-    print("Registering Observers ...")
-    subject.register_observer(email_alerts)
-    subject.register_observer(sms_alerts)
-    print("Registered Observers Successfully")
-    print("Notifying Observers ...")
-    subject.notify_observers("Alert: System is down")
-    subject.notify_observers("Alert: Server is down")
-    subject.notify_observers("Alert: Network is down")
+    # Create an inventory
+    inventory = Inventory()
+
+    # Create observers
+    console_observer1 = ConsoleObserver(inventory)
+    console_observer2 = ConsoleObserver(inventory)
+
+    # Attach observers to the inventory
+    inventory.attach(console_observer1)
+    inventory.attach(console_observer2)
+
+    # Update the inventory
+    inventory.product = "Apples"
+    inventory.quantity = 10
--- DesignPatterns Python/Structural Patterns/Decorator.py
+++ DesignPatterns Python/Structural Patterns/Decorator.py
@@ -2,80 +2,18 @@
 This python file is for the decorator design pattern
 """
 
-from abc import ABC, abstractmethod
+# most simple implementation of the decorator pattern
 
-class Component(ABC):
-    """
-    Abstract class for the component
-    """
-    @abstractmethod
-    def operation(self) -> str:
-        """
-        Abstract method for the operation
-        """
-        pass
+def simple_decorator(func):
+    def wrapper():
+        print("Something is happening before the function is called.")
+        func()
+        print("Something is happening after the function is called.")
+    return wrapper
 
-class ConcreteComponent(Component):
-    """
-    Concrete class for the component
-    """
-    def operation(self) -> str:
-        """
-        Operation for the concrete component
-        """
-        return "ConcreteComponent"
-    
-class Decorator(Component):
-    """
-    Abstract class for the decorator
-    """
-    def __init__(self, component: Component):
-        self.component = component
-        
-    @abstractmethod
-    def operation(self) -> str:
-        """
-        Abstract method for the operation
-        """
-        pass
+@simple_decorator
+def say_hello():
+    print("Hello!")
 
-class ConcreteDecoratorA(Decorator):
-    """
-    Concrete class for the decorator A
-    """
-    def operation(self) -> str:
-        """
-        Operation for the concrete decorator A
-        """
-        return f"ConcreteDecoratorA({self.component.operation()})"
-    
-class ConcreteDecoratorB(Decorator):
-    """
-    Concrete class for the decorator B
-    """
-    def operation(self) -> str:
-        """
-        Operation for the concrete decorator B
-        """
-        return f"ConcreteDecoratorB({self.component.operation()})"
-    
-def client_code(component: Component) -> None:
-    """
-    Client code for the decorator pattern
-    """
-    print(f"RESULT: {component.operation()}")
+say_hello()
 
-if __name__ == "__main__":
-    print("Welcome to Decorator Pattern")
-    print("This class aims to create a decorator class to add functionality to the component class")
-    print("Creating the concrete component")
-    component = ConcreteComponent()
-    print("Client code for the decorator pattern")
-    print("Client: I've got a simple component:")
-    client_code(component)
-    print("\n")
-    decorator1 = ConcreteDecoratorA(component)
-    decorator2 = ConcreteDecoratorB(decorator1)
-    print("Client: Now I've got a decorated component:")
-    client_code(decorator2)
-    print("End of Decorator Pattern")
